03f55b87e8183c400e28d025790b7390355b27f8
Ricardo <ricardodasilveira@gmail.com>
2022-12-01 11:47:48 -0300

    main.R

---
+++ R/main.R
@@ -0,0 +1,26 @@
+# Mapa Bothrops erythomelas
+
+# Ricardo da Silveira Filho - ricardodasilveira@gmail.com
+# Silvilene Matias - silvilenematias@gmail.com
+# 01 dez 2022
+
+
+# 0) Setup ----------------------------------------------------------------
+
+library(tidyverse)
+library(sf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
