493c562689d8940312334a31bfcea54c4c78f281
Ricardo da Silveira-Filho <ricardodasilveira@gmail.com>
2023-01-20 19:36:54 -0300

    mapa feito

--- R/main.R
+++ R/main.R
@@ -1,13 +1,14 @@
 # Mapa Bothrops erythomelas
 
-# Ricardo da Silveira Filho - ricardodasilveira@gmail.com
-# Silvilene Matias - silvilenematias@gmail.com
-# 01 dez 2022
+# Ricardo da Silveira Filho - ricardodasilveira@gmail.com - 01 dez 2020
+# Silvilene Matias - silvilenematias@gmail.com - 01 dez 2020
 
 
 # 0) Setup ----------------------------------------------------------------
 
+# Pacotes
 library(janitor)
+library(ggspatial)
 library(ggstar)
 library(parzer)
 library(patchwork)
@@ -21,9 +22,9 @@ library(sf)
 dados <- read_xlsx(path = "data/coords_Berythromelas.xlsx") %>% 
     clean_names() %>% 
     mutate(lat = parse_lat(latitude),
-           lon = parse_lon(longitude)) %>%   
+           lon = parse_lon(longitude),
+           qualif = ifelse(usados_na_qualificacao == 0, "não", "sim")) %>%   
     select(-c(latitude, longitude))
-
 
 
 # 2) Shapes ---------------------------------------------------------------
@@ -32,12 +33,15 @@ dados <- read_xlsx(path = "data/coords_Berythromelas.xlsx") %>%
 bra_adm <- sf::st_read(dsn = "/media/ricardo/backup/maps/shapes/BRA_adm/")
 # plot(bra_adm[5])
 
-# carregando biomas
+
+# Carregando biomas
 biomas <- sf::st_read(dsn = "/media/ricardo/backup/maps/shapes/biomas/", layer = "bioma") %>% 
     st_set_crs(st_crs(bra_adm))
-plot(biomas)
-cores_biomas <- c("#01665e", "#d8b365", "#f6e8c3", "#8c510a", "#c7eae5", "#5ab4ac")
+# plot(biomas)
+
+# Cores dos biomas
 # ordem: AMAZONIA, CAATINGA, CERRADO, PAMPA, PANTANAL, MATA ATLANTICA
+cores_biomas <- c("#01665e", "#d8b365", "#f6e8c3", "#8c510a", "#c7eae5", "#5ab4ac")
 
 
 # Carregando shape Mundo
@@ -45,55 +49,100 @@ mundo <- sf::st_read(dsn = "/media/ricardo/backup/maps/shapes/world_high/")
 # plot(mundo)
 
 
-# 3) Plot simples ---------------------------------------------------------
+# Carregando Rio S Francisco
+rsf <- sf::st_read("/media/ricardo/backup/maps/shapes/RSFrancisco/") %>% 
+    filter(Name == "Rio São Francisco")
+# plot(rsf[1])
 
-# a <- 
-ggplot() +
+
+# 3) Plot ----------------------------------------------------------------
+
+# mapa geral
+a <- ggplot() +
     geom_sf(data = mundo, size = 0.6, color = "black", fill = "white") +
     geom_sf(data = bra_adm, alpha = 0.2, size = 0.2, color = "black",
             fill = "#6A956D") +
     geom_sf(data = biomas, alpha = 0.4, color = NA,
             fill = cores_biomas) +
+    geom_sf(data = rsf, color = "dodgerblue2", alpha = 0.4, linewidth = 1) +
     coord_sf(xlim = c(-95, -35), ylim = c(10, -53),
               crs = "EPSG:4674") +
-    annotate(geom = "rect", xmin = -33, xmax = -49, ymin = -20, ymax = -2,
+    annotate(geom = "rect", xmin = -33, xmax = -50, ymin = -20, ymax = -2,
              alpha = 0.2, color = "red", fill = NA) +
     theme_minimal() +
     theme(panel.background = element_rect("#edf8fb"),
           axis.text = element_blank())
 
 
-
-# b <- 
-ggplot() +
+# Mapa com zoom
+b <- ggplot() +
     geom_sf(data = mundo, size = 0.6, color = "black", fill = "white") +
     geom_sf(data = bra_adm, alpha = 0.2, size = 0.2, color = "black",
             fill = "#6A956D") +
     geom_sf(data = biomas, alpha = 0.4, color = NA,
             fill = cores_biomas) +
-    coord_sf(xlim = c(-30, -49), ylim = c(-20, -2), #
+    geom_sf(data = rsf, color = "dodgerblue2", alpha = 0.4, linewidth = 2) +
+    coord_sf(xlim = c(-30, -49), ylim = c(-20, -2), 
              crs = "EPSG:4674") +
-    geom_point(data = dados, aes(x = lon, y = lat), 
-               size = dados$total + 2,
-               color = ifelse(str_detect(dados$especie, "lutzi"), "#5E262D", "#B21E00"),
-               shape = ifelse(dados$usados_na_qualificacao == 0, 1, 16)) +
-    geom_star(aes(x = -40.509070, y = -9.425430), size = 3,
-              fill = "red") +
-    labs(x = "Longitude", 
-         y = "Latitude") +
+    geom_point(data = dados, aes(x = lon, y = lat,
+                                 color = especie,
+                                 size = total,
+                                 shape = qualif)) +
+    scale_color_manual(name = "Espécie",
+                       values = c("#de2d26", "#980043"),
+                       labels = c(expression(italic("B. erythromelas")), 
+                                  expression(italic("B. lutzi")))) +
+    scale_size(name = "Quantidade de amostras",
+               breaks = c(1, 3, 5),
+               range = c(1, 10)) +
+    scale_shape_manual(name = "Utilizada na qualificação",
+                values = c(16, 1),
+                labels = c("Sim", "Não")) +
+    geom_star(aes(x = -40.509070, y = -9.425430), size = 3, fill = "red") +
+    annotation_scale(location = "tr",
+                     bar_cols = c("grey60", "white")) +
+    annotation_north_arrow(location = "tr", 
+                                      which_north = "true",
+                                      pad_x = unit(0.95, "in"), 
+                                      pad_y = unit(0.6, "in"),
+                                      style = ggspatial::north_arrow_nautical(
+                                          fill = c("grey40", "white"),
+                                          line_col = "grey20")) +
+    annotate(geom = "rect", xmin = -32.3, xmax = -33, ymin = -12.6, ymax = -12.4,
+             alpha = 0.6, fill = "dodgerblue2") +
+    annotate(geom = "text", x = -30.85, y = -12.5,
+             label = "Rio São Francisco") +
+    geom_star(aes(x = -33.25, y = -13.17), 
+              size = 3, fill = "red") +
+    annotate(geom = "text", x = -31.25, y = -13.2, 
+             label = expression(paste("Holótipo ", italic("B. erythomelas")))) +
+    labs(x = "Longitude", y = "Latitude") +
     theme_minimal() +
-    theme(panel.background = element_rect("#edf8fb"))
+    guides(color = guide_legend(override.aes = list(size = 4)),
+           shape = guide_legend(override.aes = list(size = 4))) +
+    theme(panel.background = element_rect("#edf8fb"),
+          legend.position = "right",
+          axis.text = element_text(size = 13),
+          axis.title = element_text(size = 15),
+          # legend.text = element_text(size = 13),
+          legend.title = element_text(size = 15),
+          legend.text.align = 0)
 
 
+# Unindo os dois mapas
+b + inset_element(a, left = 0.5, bottom = 0.05, right = 0.855, top = 0.4, align_to = "full")
 
+# Salvar em *.png
+ggsave("output/map_Berythomelas.png",
+       width = 15,
+       height = 15,
+       device = "png")
 
-b + inset_element(a, left = 0.3, bottom = 0.034, right = 1.009, top = 0.3, align_to = "full")
-
-
-
-
-
-
+# Salvar em formato vetorial (*.eps)
+# ggsave("output/map_Berythomelas.svg",
+#        width = 15,
+#        height = 15,
+#        device = "svg")
 
 
 
